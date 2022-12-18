29825ebaa82ef7395a4c6e000957ef80f1aee1dd
DevExpressExampleBot <fakeEmail>
2022-12-18 19:50:57 +0400

    Source auto update [skip ci]

--- VB/MapSearch/MainWindow.xaml.vb
+++ VB/MapSearch/MainWindow.xaml.vb
@@ -1,103 +1,81 @@
-﻿Imports Microsoft.VisualBasic
 Imports DevExpress.Xpf.Map
 Imports System
 Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports System.Windows.Threading
 
 Namespace MapSearch
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Private idx As Integer = 0
-		Private Delegate Sub DoSearch()
-		Private operation As DispatcherOperation
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
+        Private idx As Integer = 0
 
-		Private Sub button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			idx = 0
-			operation = Application.Current.Dispatcher.BeginInvoke(CType(AddressOf SearchAsync, DoSearch))
-		End Sub
+        Friend Delegate Sub DoSearch()
 
+        Private operation As DispatcherOperation
 
-		Private addresses As New List(Of String) From {"505 N. Brand Blvd, Glendale CA 91203, USA", "1111 N Brand Blvd, Glendale, CA 91202, USA", "300 N Brand Blvd, Glendale, CA 91203, USA"}
-		Private Sub SearchAsync()
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-			If idx < addresses.Count Then
-'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
-'ORIGINAL LINE: searchProvider.Search(addresses[idx++]);
-				searchProvider.Search(addresses(idx))
-				idx += 1
-			End If
-		End Sub
+        Private Sub button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            idx = 0
+            operation = Application.Current.Dispatcher.BeginInvoke(CType(AddressOf SearchAsync, DoSearch))
+        End Sub
 
-		Private Sub searchProvider_SearchCompleted(ByVal sender As Object, ByVal e As BingSearchCompletedEventArgs)
-			Dim result As SearchRequestResult = e.RequestResult
-			If result.ResultCode = RequestResultCode.Success Then
-				Dim regions As List(Of LocationInformation) = result.SearchResults
-				For Each region As LocationInformation In regions
-					AddPushpin(region.Location)
-					If idx = addresses.Count Then
-						map.ZoomToFitLayerItems()
-					End If
-				Next region
+        Private addresses As List(Of String) = New List(Of String) From {"505 N. Brand Blvd, Glendale CA 91203, USA", "1111 N Brand Blvd, Glendale, CA 91202, USA", "300 N Brand Blvd, Glendale, CA 91203, USA"}
 
-				DisplayResults(e.RequestResult)
-				operation = Application.Current.Dispatcher.BeginInvoke(CType(AddressOf SearchAsync, DoSearch))
-			End If
+        Private Sub SearchAsync()
+            If idx < addresses.Count Then Me.searchProvider.Search(addresses(Math.Min(Threading.Interlocked.Increment(idx), idx - 1)))
+        End Sub
 
-			If result.ResultCode = RequestResultCode.BadRequest Then
-				tbResults.Text &= "The Bing Search service does not work for this location."
-			End If
+        Private Sub searchProvider_SearchCompleted(ByVal sender As Object, ByVal e As BingSearchCompletedEventArgs)
+            Dim result As SearchRequestResult = e.RequestResult
+            If result.ResultCode = RequestResultCode.Success Then
+                Dim regions As List(Of LocationInformation) = result.SearchResults
+                For Each region As LocationInformation In regions
+                    AddPushpin(region.Location)
+                    If idx = addresses.Count Then Me.map.ZoomToFitLayerItems()
+                Next
 
-		End Sub
+                DisplayResults(e.RequestResult)
+                operation = Application.Current.Dispatcher.BeginInvoke(CType(AddressOf SearchAsync, DoSearch))
+            End If
 
-		Private Sub AddPushpin(ByVal geoPoint As GeoPoint)
-			Dim pin As New MapPushpin()
-			pin.Location = geoPoint
-			pushpins.Items.Add(pin)
-		End Sub
+            If result.ResultCode = RequestResultCode.BadRequest Then Me.tbResults.Text += "The Bing Search service does not work for this location."
+        End Sub
 
-		Private Sub NavigateTo(ByVal geoPoint As GeoPoint)
-			map.CenterPoint = geoPoint
-			map.ZoomLevel = 15
-		End Sub
+        Private Sub AddPushpin(ByVal geoPoint As GeoPoint)
+            Dim pin As MapPushpin = New MapPushpin()
+            pin.Location = geoPoint
+            Me.pushpins.Items.Add(pin)
+        End Sub
 
-		Private Sub DisplayResults(ByVal requestResult As SearchRequestResult)
-			Dim resultList As New StringBuilder("")
+        Private Sub NavigateTo(ByVal geoPoint As GeoPoint)
+            Me.map.CenterPoint = geoPoint
+            Me.map.ZoomLevel = 15
+        End Sub
 
-			If requestResult.ResultCode = RequestResultCode.Success Then
-				Dim resCounter As Integer = 1
-				For Each resultInfo As LocationInformation In requestResult.SearchResults
-					resultList.Append(String.Format(vbLf & " Result {0}:  " & vbLf, resCounter))
-					resultList.Append(String.Format(resultInfo.DisplayName & vbLf))
-					resultList.Append(String.Format("Geographical coordinates:  {0}", resultInfo.Location))
-					resultList.Append(String.Format(vbLf & "______________________________" & vbLf))
-					resCounter += 1
-				Next resultInfo
+        Private Sub DisplayResults(ByVal requestResult As SearchRequestResult)
+            Dim resultList As StringBuilder = New StringBuilder("")
+            If requestResult.ResultCode = RequestResultCode.Success Then
+                Dim resCounter As Integer = 1
+                For Each resultInfo As LocationInformation In requestResult.SearchResults
+                    resultList.Append(String.Format(Microsoft.VisualBasic.Constants.vbLf & " Result {0}:  " & Microsoft.VisualBasic.Constants.vbLf, resCounter))
+                    resultList.Append(String.Format(resultInfo.DisplayName & Microsoft.VisualBasic.Constants.vbLf))
+                    resultList.Append(String.Format("Geographical coordinates:  {0}", resultInfo.Location))
+                    resultList.Append(String.Format(Microsoft.VisualBasic.Constants.vbLf & "______________________________" & Microsoft.VisualBasic.Constants.vbLf))
+                    resCounter += 1
+                Next
+            End If
 
-
-			End If
-			tbResults.Text += resultList.ToString()
-		End Sub
-
-
-	End Class
+            Me.tbResults.Text += resultList.ToString()
+        End Sub
+    End Class
 End Namespace
